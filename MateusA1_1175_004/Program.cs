52f41bb9c3eceaf5d8c7d35c8d7c4f1b70453ae0
Mateus Abdallah <abdallahmateus@gmail.com>
2022-09-26 17:22:11 -0700

    adding new method that return the minutes

--- MateusA1_1175_004/Program.cs
+++ MateusA1_1175_004/Program.cs
@@ -1,58 +1,70 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Diagnostics;
+using System.Net.Sockets;
+using MateusA1_1175_004;
 using static System.Console;
 
 internal class Program
 {
     private static void Main(string[] args)
-    {
+    {   
         DisplayMotto();
         DisplayInstructions();
         GetFullName();
         GetAddress();
-
-        static void DisplayInstructions()
-        {
-            WriteLine("This application computes rent for Kayak and Canoe.\n" +
-            "It asks a customer to enter his/her first name, last name, \naddress and number of minutes he/she wanted to rent a kayak and canoe.\n" +
-            "The application calculates the rental price of kayak and canoe and displays that.\n" +
-            "Two tests will be performed.\n");
-        } 
+        GetMinutes();
         
-        static void DisplayMotto()
-        {    
-            WriteLine("\t\tSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
-            WriteLine("\t\tS\t\t\t\t\tS");
-            WriteLine("\t\tS" + "     Sam's makes it fun in the sun.    " + "S");                
-            WriteLine("\t\tS\t\t\t\t\tS");
-            WriteLine("\t\tSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS\n");
-        }
 
-        static string GetFullName()
-        {
-            WriteLine("Enter your first name:");
-            string firstName = ReadLine();
-
-            WriteLine("Enter your last name:");
-            string lastName = ReadLine();
-
-            string fullName = firstName + lastName;
-            WriteLine(fullName);
-
-            return fullName;
-        }
-
-        static string GetAddress()
-        {
-            WriteLine("Enter your address:");
-            string address = ReadLine();
-            WriteLine(address);
-
-            return address;
-        }
-
-
+        
         ReadKey();
     }
+
+    static void DisplayMotto()
+    {
+        WriteLine("\t\tSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+        WriteLine("\t\tS\t\t\t\t\tS");
+        WriteLine("\t\tS" + "    Sam's makes it fun in the sun.    " + " S");
+        WriteLine("\t\tS\t\t\t\t\tS");
+        WriteLine("\t\tSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS\n");
+    }
+
+    static void DisplayInstructions()
+    {
+        WriteLine("This application computes rent for Kayak and Canoe.\n" +
+        "It asks a customer to enter his/her first name, last name, \naddress and number of minutes he/she wanted to rent a kayak and canoe.\n" +
+        "The application calculates the rental price of kayak and canoe and displays that.\n" +
+        "Two tests will be performed.\n");
+    }
+
+    static string GetFullName()
+    {
+        WriteLine("Enter your first name:");
+        string firstName = ReadLine();
+
+        WriteLine("Enter your last name:");
+        string lastName = ReadLine();
+
+        string fullName = firstName + lastName;
+
+        return fullName;
+    }
+
+    static string GetAddress()
+    {
+        WriteLine("Enter your address:");
+        string address = ReadLine();
+        
+        return address;
+    }
+
+    static int GetMinutes()
+    {
+        WriteLine("Enter how many minutes you want:");
+        string minutes = ReadLine();
+        int minutesInt = int.Parse(minutes);
+
+        return minutesInt;
+    }
+
 
 }
---
+++ MateusA1_1175_004/RentalApp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace MateusA1_1175_004
+{
+    internal class RentalApp
+    {
+        private string firstName;
+        private string lastName;
+        private string address;
+        private int numMins;
+
+        public RentalApp(string firstName, string lastName, string address, int numMins)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.numMins = numMins;
+        }
+
+        
+    }
+}
