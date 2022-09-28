d999fed8143124e40210c4283ec4183c34ae290e
Mateus Abdallah <abdallahmateus@gmail.com>
2022-09-27 19:45:19 -0700

    transfer the methods inside the class RentalApp

--- MateusA1_1175_004/Program.cs
+++ MateusA1_1175_004/Program.cs
@@ -7,64 +7,20 @@ using static System.Console;
 internal class Program
 {
     private static void Main(string[] args)
-    {   
-        DisplayMotto();
-        DisplayInstructions();
-        GetFullName();
-        GetAddress();
-        GetMinutes();
-        
+    {
+        RentalApp rentalApp = new RentalApp();
+
+        rentalApp.DisplayMotto();
+        rentalApp.DisplayInstructions();
+        rentalApp.GetFullName();
+        rentalApp.GetAddress();
+        rentalApp.GetMinutes();
 
         
         ReadKey();
     }
 
-    static void DisplayMotto()
-    {
-        WriteLine("\t\tSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
-        WriteLine("\t\tS\t\t\t\t\tS");
-        WriteLine("\t\tS" + "    Sam's makes it fun in the sun.    " + " S");
-        WriteLine("\t\tS\t\t\t\t\tS");
-        WriteLine("\t\tSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS\n");
-    }
 
-    static void DisplayInstructions()
-    {
-        WriteLine("This application computes rent for Kayak and Canoe.\n" +
-        "It asks a customer to enter his/her first name, last name, \naddress and number of minutes he/she wanted to rent a kayak and canoe.\n" +
-        "The application calculates the rental price of kayak and canoe and displays that.\n" +
-        "Two tests will be performed.\n");
-    }
-
-    static string GetFullName()
-    {
-        WriteLine("Enter your first name:");
-        string firstName = ReadLine();
-
-        WriteLine("Enter your last name:");
-        string lastName = ReadLine();
-
-        string fullName = firstName + lastName;
-
-        return fullName;
-    }
-
-    static string GetAddress()
-    {
-        WriteLine("Enter your address:");
-        string address = ReadLine();
-        
-        return address;
-    }
-
-    static int GetMinutes()
-    {
-        WriteLine("Enter how many minutes you want:");
-        string minutes = ReadLine();
-        int minutesInt = int.Parse(minutes);
-
-        return minutesInt;
-    }
 
 
 }
--- MateusA1_1175_004/RentalApp.cs
+++ MateusA1_1175_004/RentalApp.cs
@@ -9,19 +9,54 @@ namespace MateusA1_1175_004
 {
     internal class RentalApp
     {
-        private string firstName;
-        private string lastName;
-        private string address;
-        private int numMins;
+        
+         public void DisplayMotto()
+         {
+            WriteLine("\t\tSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+            WriteLine("\t\tS\t\t\t\t\tS");
+            WriteLine("\t\tS" + "    Sam's makes it fun in the sun.    " + " S");
+            WriteLine("\t\tS\t\t\t\t\tS");
+            WriteLine("\t\tSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS\n");
+         }
 
-        public RentalApp(string firstName, string lastName, string address, int numMins)
+        public void DisplayInstructions()
         {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.address = address;
-            this.numMins = numMins;
+            WriteLine("This application computes rent for Kayak and Canoe.\n" +
+            "It asks a customer to enter his/her first name, last name, \naddress and number of minutes he/she wanted to rent a kayak and canoe.\n" +
+            "The application calculates the rental price of kayak and canoe and displays that.\n" +
+            "Two tests will be performed.\n");
         }
 
-        
+        public string GetFullName()
+        {
+            WriteLine("Enter your first name:");
+            string firstName = ReadLine();
+
+            WriteLine("Enter your last name:");
+            string lastName = ReadLine();
+
+            string fullName = firstName + lastName;
+
+            return fullName;
+        }
+
+        public string GetAddress()
+        {
+            WriteLine("Enter your address:");
+            string address = ReadLine();
+
+            return address;
+        }
+
+        public int GetMinutes()
+        {
+            WriteLine("Enter how many minutes you want:");
+            string minutes = ReadLine();
+            int minutesInt = int.Parse(minutes);
+
+            return minutesInt;
+        }
+
+
     }
 }
