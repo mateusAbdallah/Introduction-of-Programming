6348fb37929b01d6f305d5fbb038edd532dec21a
Mateus Abdallah <abdallahmateus@gmail.com>
2022-10-23 23:40:15 -0700

    refactoring the code and new data members, propreties, methods.

--- MateusA1_1175_004/Program.cs
+++ MateusA1_1175_004/Program.cs
@@ -8,19 +8,60 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        //instantiating the object
-        RentalApp rentalApp = new RentalApp();
 
-        rentalApp.DisplayMotto();
-        rentalApp.DisplayInstructions();
-        rentalApp.DisplayDetails();
+        DisplayMotto();
+        DisplayInstructions();
+
+        WriteLine("Enter you first and last name: ");
+        string name = ReadLine();
+
+        WriteLine("Enter a type do you want (canoe or kayak): ");
+        string type = ReadLine();
+
+        WriteLine("For how many minutes to you to rent: ");
+        int minutes = int.Parse(ReadLine());
+
+        
+        //instantiating the object
+
+        //WriteLine("\t\tSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+        //WriteLine("\t\tS\t\t\t\t\tS");
+        //WriteLine("\t\tS" + "    Sam's makes it fun in the sun.    " + " S");
+        //WriteLine("\t\tS\t\t\t\t\tS");
+        //WriteLine("\t\tSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS\n");
+
+        //RentalApp rentalApp = new RentalApp();
+
+        //rentalApp.DisplayMotto();
+        //rentalApp.DisplayInstructions();
+        //rentalApp.DisplayDetails();
 
         ReadKey();
     }
 
-     
+    static void DisplayMotto()
+    {
+        WriteLine("\t\tSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+        WriteLine("\t\tS\t\t\t\t\tS");
+        WriteLine("\t\tS" + "    Sam's makes it fun in the sun.    " + " S");
+        WriteLine("\t\tS\t\t\t\t\tS");
+        WriteLine("\t\tSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS\n");
+    }
 
+    static void DisplayInstructions()
+    {
+        WriteLine("This application computes rent for Kayak and Canoe.\n" +
+       "It asks a customer to enter his/her first name, last name, \naddress and number of minutes he/she wanted to rent a kayak and canoe.\n" +
+       "The application calculates the rental price of kayak and canoe and displays that.\n" +
+       "Two tests will be performed.\n");
 
-
+    }
 
 }
+
+
+
+
+
+
+
--- MateusA1_1175_004/RentalApp.cs
+++ MateusA1_1175_004/RentalApp.cs
@@ -9,75 +9,108 @@ namespace MateusA1_1175_004
 {
     internal class RentalApp
     {   
+        private string name;
+        //private int minutes;
+        private string type;
+
+        public RentalApp(string n)
+        {
+            name = n;
+                        
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public double CalculateTotal(int d)
+        {
+            double rent = 0.0;
+            if(type == "Canoe" || type == "canoe")
+            {
+                rent = 12.99;
+            }
+            if(type == "kayak" || type == "Kayak")
+            {
+                rent = 15.99;
+            }
+            
+            return rent * d;
+        }
+
 
         //pattern message from the application
-         public void DisplayMotto()
-         {
-            WriteLine("\t\tSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
-            WriteLine("\t\tS\t\t\t\t\tS");
-            WriteLine("\t\tS" + "    Sam's makes it fun in the sun.    " + " S");
-            WriteLine("\t\tS\t\t\t\t\tS");
-            WriteLine("\t\tSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS\n");
-         }
+         //public void DisplayMotto()
+         //{
+         //   WriteLine("\t\tSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+         //   WriteLine("\t\tS\t\t\t\t\tS");
+         //   WriteLine("\t\tS" + "    Sam's makes it fun in the sun.    " + " S");
+         //   WriteLine("\t\tS\t\t\t\t\tS");
+         //   WriteLine("\t\tSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS\n");
+         //}
         //pattern message from the application
-        public void DisplayInstructions()
-        {
-            WriteLine("This application computes rent for Kayak and Canoe.\n" +
-            "It asks a customer to enter his/her first name, last name, \naddress and number of minutes he/she wanted to rent a kayak and canoe.\n" +
-            "The application calculates the rental price of kayak and canoe and displays that.\n" +
-            "Two tests will be performed.\n");
-        }
+        //public void DisplayInstructions()
+        //{
+        //    WriteLine("This application computes rent for Kayak and Canoe.\n" +
+        //    "It asks a customer to enter his/her first name, last name, \naddress and number of minutes he/she wanted to rent a kayak and canoe.\n" +
+        //    "The application calculates the rental price of kayak and canoe and displays that.\n" +
+        //    "Two tests will be performed.\n");
+        //}
         
         //getting the user's full name
-        public string GetFullName()
-        {
-            WriteLine("Please enter your first name: ");
-            string firstName = ReadLine();
+        //public string GetFullName()
+        //{
+        //    WriteLine("Please enter your first name: ");
+        //    string firstName = ReadLine();
 
-            WriteLine("Please enter your last name: ");
-            string lastName = ReadLine();
+        //    WriteLine("Please enter your last name: ");
+        //    string lastName = ReadLine();
 
-            string fullName = firstName + " " + lastName;
+        //    string fullName = firstName + " " + lastName;
             
 
-            return fullName;
-        }
+        //    return fullName;
+        //}
         //getting the user's address
-        public string GetAddress()
-        {
-            WriteLine("Please enter your address:");
-            string address = ReadLine();
+        //public string GetAddress()
+        //{
+        //    WriteLine("Please enter your address:");
+        //    string address = ReadLine();
 
-            return address;
-        }
+        //    return address;
+        //}
 
 
         //getting the user's minutes
-        public int GetMinutes()
-        {
-            WriteLine("Please enter how many minutes you want:");
-            string minutes = ReadLine();
-            int minutesInt = int.Parse(minutes);
+        //public int GetMinutes()
+        //{
+        //    WriteLine("Please enter how many minutes you want:");
+        //    string minutes = ReadLine();
+        //    int minutesInt = int.Parse(minutes);
 
-            return minutesInt;
-        }
+        //    return minutesInt;
+        //}
+
+        
 
 
         //display the infos in the console
-        public void DisplayDetails()
-        {
-            string name = GetFullName();
-            string address = GetAddress();
-            int minutes = GetMinutes();
+        //public void DisplayDetails()
+        //{
+        //    string name = GetFullName();
+        //    string address = GetAddress();
+        //    int minutes = GetMinutes();
             
 
 
-            WriteLine("\nSam's rental App");
-            WriteLine("Customer name: " + name + " ");
-            WriteLine("Customer address: " + address);
-            WriteLine("Minutes would be rented: " + minutes);
+        //    WriteLine("\nSam's rental App");
+        //    WriteLine("Customer name: " + name + " ");
+        //    WriteLine("Customer address: " + address);
+        //    WriteLine("Minutes would be rented: " + minutes);
             
-        }
+        //}
         
 
 
